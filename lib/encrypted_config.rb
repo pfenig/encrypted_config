389c21bb799ac287095fe0936b0ce20c8af33415
Geoff <geoff@altamotors.co>
2015-11-19 15:14:19 -0800

    Write a base64'd version of encrypted config for use in json

--- lib/encrypted_config.rb
+++ lib/encrypted_config.rb
@@ -3,7 +3,7 @@ require 'iron_worker'
 
 module EncryptedConfig
   def self.decrypt(encrypted, keys_path=nil)
-    decipher = OpenSSL::Cipher::AES.new(128, :CBC)
+    decipher = OpenSSL::Cipher::AES256.new(:CBC)
     decipher.decrypt
     decipher.key = File.read(
       (keys_path && File.join(keys_path, 'config.key')) || File.expand_path('config.key')
